6f4d40828df01d54a7f3b4fce20627333014c50f
haonanyu <haonanyu@baidu.com>
2017-08-25 15:18:01 -0700

    fix opencv imread issue when BUILD_TYPE is inconsistent; update find glog and gflags

--- cmake/FindGlog.cmake
+++ cmake/FindGlog.cmake
@@ -13,28 +13,20 @@ include(FindPackageHandleStandardArgs)
 
 set(GLOG_ROOT_DIR "" CACHE PATH "Folder contains Google glog")
 
-if(WIN32)
-    find_path(GLOG_INCLUDE_DIR glog/logging.h
-        PATHS ${GLOG_ROOT_DIR}/src/windows)
+if (NOT GLOG_ROOT_DIR)
+  find_path(GLOG_INCLUDE_DIR glog/logging.h)
+  find_library(GLOG_LIBRARY glog
+    PATH_SUFFIXES lib lib64)
 else()
-    find_path(GLOG_INCLUDE_DIR glog/logging.h
-        PATHS ${GLOG_ROOT_DIR}/include)
-endif()
-
-if(MSVC)
-    find_library(GLOG_LIBRARY_RELEASE libglog_static
-        PATHS ${GLOG_ROOT_DIR}
-        PATH_SUFFIXES Release)
-
-    find_library(GLOG_LIBRARY_DEBUG libglog_static
-        PATHS ${GLOG_ROOT_DIR}
-        PATH_SUFFIXES Debug)
-
-    set(GLOG_LIBRARY optimized ${GLOG_LIBRARY_RELEASE} debug ${GLOG_LIBRARY_DEBUG})
-else()
-    find_library(GLOG_LIBRARY glog
-        PATHS ${GLOG_ROOT_DIR}
-        PATH_SUFFIXES lib lib64)
+  find_path(GLOG_TMP_INCLUDE_DIR glog/logging.h
+    PATHS ${GLOG_ROOT_DIR}/include
+    NO_DEFAULT_PATH)
+  set(GLOG_INCLUDE_DIR ${GLOG_TMP_INCLUDE_DIR})
+  find_library(GLOG_TMP_LIBRARY glog
+    PATHS ${GLOG_ROOT_DIR}
+    NO_DEFAULT_PATH
+    PATH_SUFFIXES lib lib64)
+  set(GLOG_LIBRARY ${GLOG_TMP_LIBRARY})
 endif()
 
 find_package_handle_standard_args(Glog DEFAULT_MSG GLOG_INCLUDE_DIR GLOG_LIBRARY)
